84c2b9edb534d0a04fd796b0ed6547fef4927f4e
Maggie Ryder <maggieryder@mac.com>
2019-08-27 07:10:51 -0400

    small update to groupBtn

--- src/components/ui/btnGroup.js
+++ src/components/ui/btnGroup.js
@@ -4,7 +4,7 @@ import classes from './ui.module.scss'
 
 const BtnGroup = ({ items, clickHandler }) => (
     <div className={classes['btn-group']}>
-        { items.map((item,i) => <button key={i} onClick={clickHandler} className={classes['btn']}>item</button>) }
+        { items.map((item,i) => <button key={i} onClick={clickHandler} className={classes['btn']}>{item + 1}</button>) }
     </div>
 )
 
