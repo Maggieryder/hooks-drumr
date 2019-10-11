ae78526f7c6622a3bfbf305a42d77f057b18e18f
Maggie Ryder <maggieryder@mac.com>
2019-10-10 22:35:06 -0400

    clean up

--- src/components/barDisplay.js
+++ src/components/barDisplay.js
@@ -1,8 +1,5 @@
 import React, { useContext } from 'react'
 
-
-// import useSequencer from '../hooks/useSequencer'
-
 import { DrumrContext } from '../context/DrumrContext'
 
 import vars from '../scss/_vars.scss';
@@ -10,14 +7,9 @@ import classes from './barDisplay.module.scss'
 
 const BarDisplay = () => {
 
-    // const { numBeats, numSteps, currentBar, currentStep, tempo, isPlaying } = useSequencer();
-    const {state:{ sequencer: { numBars, currentBar, isPlaying }}} = useContext(DrumrContext)
+    const {state:{ sequencer: { numBars, currentBar }}} = useContext(DrumrContext)
 
     const bars = Array.from(Array(100).keys())
-
-    // const barStyle = {
-    //     color: currentBar
-    // }
 
     return (
         <div className={classes['bar-display-container']}>
@@ -28,7 +20,5 @@ const BarDisplay = () => {
         </div>
     )
 }
-
-//, background:  i < numBars ? vars.mutedBlack : vars.defaultBlack
 
 export default BarDisplay
