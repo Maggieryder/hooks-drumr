99895644ae218510f772cac424f73d32cb273797
Maggie Ryder <maggieryder@mac.com>
2019-11-05 22:41:53 -0500

    useLoadData hook

---
+++ src/hooks/useLoadData.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useLoadData = (url, options) => {
+    const [response, setResponse] = useState(null)
+    const [error, setError] = useState(null)
+    const [isLoading, setIsLoading] = useState(null)
+    useEffect(() => {
+        const loadData = async () => {
+            setIsLoading(true)
+            try {
+                const res = await axios.get(url, options)
+                const json = await res.json()
+                setResponse(json)
+                setIsLoading(false)
+            } catch (err) {
+                setError(err)
+            }
+        }
+        loadData()
+    }, [])
+    return { response, error, isLoading }
+}
+
+export default useLoadData
+
+// usage
+// const res = useLoadData("https://dog.ceo/api/breeds/image/random", {})
+// if (!res.response) {
+//     return <div>Loading...</div>
+// }
+
+// const loadData = async (url) => {  
+//     dispatch({ type: TYPES.IS_LOADING, value: true })
+//     try {
+//       const response = await axios.get(`./${url}.json`)
+//       // console.log('Success!',response.data.kits);
+//       dispatch({ type: TYPES.DATA_LOADED, value: response.data })
+//     } catch (err) {
+//       dispatch({ type: TYPES.HAS_ERROR, value: err })
+//     }
+//   }
